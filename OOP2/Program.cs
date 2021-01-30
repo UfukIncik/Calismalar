4cfc43f61988dc71f422bd374e610e829467ec8e
kennenbtc <k.ufukincik@gmail.com>
2021-01-30 14:05:29 +0300

    EnginDemirOg 5.Ders

---
+++ OOP2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demirog";
+            musteri1.TcNo = "1234567914";
+
+            //Kodlama.io
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 1;
+            musteri2.MusteriNo = " 54312";
+            musteri2.SirketAdi =  "Kodlama.io";
+            musteri2.VergiNo = " 123146789";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri2);
+            customerManager.Add(musteri3);
+
+
+
+        }
+    }
+}
---
+++ OOP3/TasitKrediManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class TasitKrediManager : IKrediManager
+    {
+        public void BiseyYap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Calculate()
+        {
+            Console.WriteLine("Tasit kredisi odeme plani hesaplandi");
+            
+        }
+    }
+}
